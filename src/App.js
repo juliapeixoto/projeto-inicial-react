fcb12714734be6e2e0f6e06e6398a51d67a6288f
juliap <juliap@ciandt.com>
2021-04-01 16:00:49 -0300

    added categoria feature

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import FormularioCadastro from './components/FormularioCadastro';
 import ListaDeNotas from "./components/ListaDeNotas";
+import ListaDeCategorias from "./components/ListaDeCategorias";
 import "./assets/App.css";
 import './assets/index.css';
 
@@ -9,12 +10,13 @@ class App extends Component {
   constructor(){
     super();
     this.state = {
-      notas:[]
+      notas:[],
+      categorias:[]
     };
   }
 
-  criarNota(titulo, texto){
-    const novaNota = {titulo, texto};
+  criarNota(titulo, texto, categoria){
+    const novaNota = {titulo, texto, categoria};
     const novoArrayNotas = [...this.state.notas,novaNota]
     const novoEstado = {
       notas:novoArrayNotas
@@ -22,12 +24,34 @@ class App extends Component {
     this.setState(novoEstado);
   }
 
+  adicionarCategoria(nomeCategoria){
+    const novoArrayCategorias = [...this.state.categorias, nomeCategoria];
+    const novoEstado = {...this.state, categorias:novoArrayCategorias};
+    this.setState(novoEstado);
+  }
+
+  deletarNota(index){
+    let arrayNotas = this.state.notas;
+    arrayNotas.splice(index, 1);
+    this.setState({notas:arrayNotas});
+  }
+
   render(){
     return (
       <section className="conteudo">
-        <FormularioCadastro criarNota={this.criarNota.bind(this)}/>
-  
-        <ListaDeNotas notas={this.state.notas}/>
+        <FormularioCadastro
+        categorias={this.state.categorias}
+        criarNota={this.criarNota.bind(this)}/>
+
+        <main className="conteudo-principal">
+          <ListaDeCategorias 
+          adicionarCategoria={this.adicionarCategoria.bind(this)}
+          categorias={this.state.categorias}/>
+
+          <ListaDeNotas apagarNota={this.deletarNota.bind(this)}
+          notas={this.state.notas}/>
+        </main>
+        
       </section>
     );
   }
